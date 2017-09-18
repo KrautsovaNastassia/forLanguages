4a8d1292fb6cd33083fd6ada0ac4f74d76773972
Kravtsova Nastassia <kravtsova.n98@yandex.by>
2017-09-18 14:55:31 +0300

    main route

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'BaseController@getIndex');
+
+//всегда последний
+Route::get('/{id}', 'BaseController@getOne');
